24b2030a81564fa815d35dc3d0d4725512742634
Steven Klise <sklise@gmail.com>
2012-10-11 11:08:09 -0400

    Start trying to find where the like button is on the page

--- content.js
+++ content.js
@@ -1,9 +1,26 @@
 likes = document.getElementsByClassName('UFILikeLink');
 
-for(i = 0; i < likes.length; i++) {
-  fireEvent(likes[i], 'click');
+for (i = 0; i < likes.length; i++) {
+  // rate limiting
+  // fireEvent(likes[i], 'click');
+  likes[i].style.color = 'red';
+  console.log(likes[i].innerHTML, findPos(likes[i]) )
 }
 
+
+// http://www.quirksmode.org/js/findpos.html
+function findPos(obj) {
+  var curleft = curtop = 0;
+
+  if (obj.offsetParent) {
+    do {
+      curleft += obj.offsetLeft;
+      curtop += obj.offsetTop;
+    } while (obj = obj.offsetParent);
+  }
+
+  return [curleft, curtop];
+}
 
 // http://jehiah.cz/a/firing-javascript-events-properly via
 // http://stackoverflow.com/questions/143747/is-it-possible-to-trigger-a-links-or-any-elements-click-event-through-javasc
